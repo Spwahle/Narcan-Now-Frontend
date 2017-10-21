6a48e59b1654a987660861306603ffe9f099e036
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-21 14:12:14 -0700

    adds app and auth

---
+++ src/components/app/index.js
@@ -0,0 +1,48 @@
+import './_app.scss';
+import React from 'react';
+import {connect} from 'react-redux';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import LandingContainer from '../landing-container';
+import {setToken} from '../../actions/auth-actions.js';
+import Header from '../header';
+
+import HostContainer from '../host-container';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='narcan'>
+          <main>
+            <Route exact path='*' component={Header} />
+            <Route exact path='/' render={() => <Redirect from='/' to='/welcome/login' />} />
+            <Route path='/welcome/:auth' component={LandingContainer} />
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+let mapStateToProps = (state) => ({
+  token: state.auth,
+  profile: state.profile
+});
+
+let mapDispatchToProps = (dispatch) => ({
+  setToken: (token) => dispatch(setToken(token))
+});
+
+App.propTypes = {
+  profile: PropTypes.object,
+  token: PropTypes.string,
+  setToken: PropTypes.func,
+  fetchProfile: PropTypes.func
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
