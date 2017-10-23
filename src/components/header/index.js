64635150ffcb27f4d6af30985ba9810b05a2151c
Tim Turner <timmyturner@me.com>
2017-10-23 11:46:13 -0700

    fixes the header issue that Shaun caused entirely by himself

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import './_header.scss';
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Narcan Now Header Thing</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
