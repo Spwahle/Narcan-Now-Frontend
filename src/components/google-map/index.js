fd605115d4e8cc48ead7ef2b5d1e5da0328cb54f
Tim Turner <timmyturner@me.com>
2017-10-24 14:12:40 -0700

    rework google map to include markers and directions in one map component

--- src/components/google-map/index.js
+++ src/components/google-map/index.js
@@ -6,61 +6,11 @@ import { compose, withProps, lifecycle } from 'recompose';
 import fetch from 'isomorphic-fetch';
 const narcanLocation = JSON.parse('{"_id": "1234567890","name": "Business Name","address": "Street Address","coordinates": "lat: 47.606, lng: -122.332"}');
 
-// class NarcanMap extends React.Component {
-//   render() {
-//     const markers = this.props.markers || [];
-//
-//     return (
-//       <div className="map-container">
-//         <GoogleMap
-//           defaultZoom={12}
-//           defaultCenter={{ lat: 47.606, lng: -122.332 }}>
-//           {markers.map((marker, index) => (
-//             <Marker {...marker} />
-//           )
-//           )}
-//         </GoogleMap>
-//       </div>
-//     );
-//   }
-// }
-
-//export default withScriptjs(withGoogleMap(NarcanMap));
-
-const MapWithAMarkerClusterer = compose(
+const MapWithAMarkerClustererAndDirectionsRenderer = compose(
   withProps({
     googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDOTFGuAM5KLro5-_3oNBKzpKYLhcVXZxg&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `800px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 47.606, lng: -122.332 }}
-  >
-    <MarkerClusterer
-      averageCenter
-      enableRetinaIcons
-      gridSize={60}
-    >
-      {props.markers.map(marker => (
-        <Marker
-          key={marker.photo_id}
-          position={{ lat: marker.latitude, lng: marker.longitude }}
-        />
-      ))}
-    </MarkerClusterer>
-  </GoogleMap>
-);
-
-const MapWithADirectionsRenderer = compose(
-  withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDOTFGuAM5KLro5-_3oNBKzpKYLhcVXZxg&v=3.exp&libraries=geometry,drawing,places',
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
@@ -70,9 +20,9 @@ const MapWithADirectionsRenderer = compose(
       const DirectionsService = new google.maps.DirectionsService();
 
       DirectionsService.route({
-        origin: new google.maps.LatLng(41.8507300, -87.6512600),
-        destination: new google.maps.LatLng(41.8525800, -87.6514100),
-        travelMode: google.maps.TravelMode.DRIVING,
+        origin: new google.maps.LatLng(47.618220, -122.351865),
+        destination: new google.maps.LatLng(47.616071, -122.344704),
+        travelMode: google.maps.TravelMode.WALKING,
       }, (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
           this.setState({
@@ -86,10 +36,22 @@ const MapWithADirectionsRenderer = compose(
   })
 )(props =>
   <GoogleMap
-    defaultZoom={7}
-    defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
+    defaultZoom={8}
+    defaultCenter={{ lat: 47.606, lng: -122.332 }}
   >
     {props.directions && <DirectionsRenderer directions={props.directions} />}
+    <MarkerClusterer
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.markers.map(marker => (
+        <Marker
+          key={marker._id}
+          position={{ lat: marker.lat, lng: marker.lng }}
+        />
+      ))}
+    </MarkerClusterer>
   </GoogleMap>
 );
 
@@ -99,20 +61,19 @@ class DemoApp extends React.PureComponent {
   }
 
   componentDidMount() {
-    const url = ['https://gist.githubusercontent.com/farrrr/dfda7dd7fccfec5474d3/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json'];
+    const url = ['https://gist.githubusercontent.com/ratiphi/596fe4d088b6e2ce48087eb10e9b9ab1/raw/6feca2a9b806a85528c1eeb2c92a20468ff32f83/data.json'];
 
     fetch(url)
     .then(res => res.json())
     .then(data => {
-      this.setState({ markers: data.photos });
+      this.setState({ markers: data.locations });
     });
   }
 
   render() {
     return (
       <div>
-        <MapWithAMarkerClusterer markers={this.state.markers} />
-        <MapWithADirectionsRenderer />
+        <MapWithAMarkerClustererAndDirectionsRenderer markers={this.state.markers} />
       </div>
     );
   }
