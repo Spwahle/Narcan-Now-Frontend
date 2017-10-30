2775aa5f910dd3fa5b56d7b21526033d6eeb77c1
Shaun Wahle <shaun.wahle@gmail.com>
2017-10-30 13:42:53 -0700

    Adds final touches

--- src/component/homepage/index.js
+++ src/component/homepage/index.js
@@ -5,10 +5,11 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className='home-text'>
+        <br />
         <h1><br />A crowd based soultion to
       getting Narcan to those in need.</h1>
         <br />
-        <br /> <h1>Please sign up for a free acount and help our community!</h1>
+        <br /> <h3>Please sign up for a free acount and help our community!</h3>
       </div>
     );
   }
